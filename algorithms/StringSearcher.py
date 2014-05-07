5c4ac03e1ab693f1b983f08d940cb5b12647b22c
atscott <atscott01@gmail.com>
2014-05-07 12:07:55 -0500

    have tests now

--- algorithms/StringSearcher.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'atscott'
-
-
-class StringSearcher(object):
-    def search(self, text, pattern):
-        raise NotImplementedError("Should have been implemented")
---
+++ test/AlgorithmVerification.py
@@ -0,0 +1,26 @@
+import unittest
+from lib.algorithms.BoyerMoore import BoyerMoore
+from lib.algorithms.BoyerMooreHorspool import BoyerMooreHorspool
+from lib.algorithms.KnuthMorrisPratt import KnuthMorrisPratt
+
+__author__ = 'andrewscott'
+
+
+class TestSearchAlgorithms(unittest.TestCase):
+    def setUp(self):
+        self.test_text = "something that I'm searching that is in more than one place."
+        self.horspool = BoyerMooreHorspool()
+        self.pratt = KnuthMorrisPratt()
+        self.moore = BoyerMoore()
+
+    def test_knuth_morris_pratt_multiple_results(self):
+        locations = list(self.pratt.search(self.test_text, "that"))
+        self.assertEquals(locations, list((10, 29)))
+
+    def test_boyer_moore_horspool_multiple_results(self):
+        locations = list(self.horspool.search(self.test_text, "that"))
+        self.assertEquals(locations, list((10, 29)))
+
+    def test_boyer_moore_multiple_results(self):
+        locations = list(self.moore.search(self.test_text, "that"))
+        self.assertEquals(locations, list((10, 29)))
