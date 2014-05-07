057f7e20b13ff08b0e3203a61d4acd689fec3887
atscott <atscott01@gmail.com>
2014-05-07 12:08:22 -0500

    add missing files

---
+++ lib/algorithms/BoyerMooreHorspool.py
@@ -0,0 +1,27 @@
+from lib.algorithms.StringSearcher import StringSearcher
+
+__author__ = 'atscott'
+
+
+class BoyerMooreHorspool(StringSearcher):
+    def search(self, text, pattern):
+        m = len(pattern)
+        n = len(text)
+        if m > n:
+            return
+        skip = []
+        for k in range(256):
+            skip.append(m)
+        for k in range(m - 1):
+            skip[ord(pattern[k])] = m - k - 1
+        skip = tuple(skip)
+        k = m - 1
+        while k < n:
+            j = m - 1
+            i = k
+            while j >= 0 and text[i] == pattern[j]:
+                j -= 1
+                i -= 1
+            if j == -1:
+                yield i + 1
+            k += skip[ord(text[k])]
