86043b0f1bbd9baef7ef0a4414b18567de8151fa
atscott <atscott01@gmail.com>
2014-05-09 10:49:18 -0500

    changed naive to be generator function

--- lib/algorithms/NaiveSearch.py
+++ lib/algorithms/NaiveSearch.py
@@ -5,18 +5,16 @@ __author__ = 'https://github.com/ahelwer/IvoryTower/tree/master/string_algo'
 
 class NaiveSearch(StringSearcher):
     def search(self, text, pattern):
-        matches = []
         if len(pattern) == 0 or len(text) < len(pattern):
-            return matches
+            return
         k = 0  # Alignment of P relative to T
         while k + len(pattern) <= len(text):
             i = 0  # Index to be compared
             while i < len(pattern) and pattern[i] == text[k + i]:
                 i += 1
             if i == len(pattern):  # Match found
-                matches.append(k)
+                yield k
             k += 1
-        return matches
 
     def __str__(self):
         return 'Naive'
