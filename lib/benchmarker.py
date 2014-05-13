b467f4cab0badd1aa4920d757e2c6b0b2e118507
atscott <atscott01@gmail.com>
2014-05-12 20:28:56 -0500

    writing to csv

--- lib/benchmarker.py
+++ lib/benchmarker.py
@@ -2,6 +2,7 @@ from lib.algorithms.BoyerMooreHorspool import BoyerMooreHorspool
 from lib.algorithms.KnuthMorrisPratt import KnuthMorrisPratt
 from lib.algorithms.NaiveSearch import NaiveSearch
 from lib.algorithms.ZSearch import ZSearch
+import os.path
 import time
 
 __author__ = 'atscott'
@@ -16,21 +17,28 @@ class StringSearchBenchmarker():
 
     def benchmark(self):
         self.print_header()
+        file = open('results.csv', 'a+')
         for text_to_search in self.various_sized_aesops:
             csv_line = str(len(text_to_search)) + ',' + \
-                str(len('he')) + ',' + \
-                str(len(list(BoyerMooreHorspool().search(text_to_search, 'qu'))))
+                       str(len('he')) + ',' + \
+                       str(len(list(BoyerMooreHorspool().search(text_to_search, 'qu'))))
             for algorithm in self.search_algorithms:
                 start_time_in_millis = current_milli_time()
-                locations = list(algorithm.search(text_to_search, 'qu'))
+                list(algorithm.search(text_to_search, 'qu'))
                 end_time_in_millis = current_milli_time()
                 csv_line += ',' + str(end_time_in_millis - start_time_in_millis)
+            file.write(csv_line + '\n')
             print(csv_line)
+        file.close
 
     def print_header(self):
         header = 'length of search text, length of pattern, occurrences'
         for algorithm in self.search_algorithms:
             header += ',' + str(algorithm)
+        if not os.path.isfile('results.csv'):
+            f = open('results.csv', 'w')
+            f.write(header + '\n')
+            f.close()
         print(header)
 
     def __generate_items_to_search(self):
